1475b91bf6221f56a0b9a41baf3faa70d5e29920
mateoquaglia <117778104+mateoquaglia@users.noreply.github.com>
2024-05-06 19:45:17 -0300

    Update entities.js

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -173,6 +173,7 @@ game.PipeGenerator = me.Renderable.extend({
             var hit = new me.pool.pull("hit", this.posX, hitPos);
             ////////////////////////////////////////////////////////////////////////////////////////////////////
             var nonCollidingEntity = new game.NonCollidingEntity(this.posX, posY + pipeHoleSize / 2);
+            ////////////////////////////////////////////////////////////////////////////////////////////////////
             pipe1.renderable.currentTransform.scaleY(-1);
             me.game.world.addChild(pipe1, 10);
             me.game.world.addChild(pipe2, 10);
