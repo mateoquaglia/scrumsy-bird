d0eec14be39fb00c9901569c7d8897a7c98af7ae
mateoquaglia <117778104+mateoquaglia@users.noreply.github.com>
2024-05-07 15:00:51 -0300

    Update babel.config.js

--- test/babel.config.js
+++ test/babel.config.js
@@ -1,1 +1,15 @@
-
+module.exports = {
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-react',
+      '@babel/preset-flow',
+      "@babel/preset-typescript"
+    ],
+    transform: {
+        '^.+\\.jsx?$': 'babel-jest',
+      },
+    plugins: [
+      'babel-plugin-styled-components',
+      '@babel/plugin-proposal-class-properties',
+    ]
+  }
