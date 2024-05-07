7e5ec6f39a83ba497a3a43973724bc69cc1fdee4
mateoquaglia <117778104+mateoquaglia@users.noreply.github.com>
2024-05-07 15:01:30 -0300

    Update jest.config.js

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,1 +1,14 @@
-
+module.exports = {
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    preset: 'ts-jest',
+   testEnvironment: 'node',
+    testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+    testEnvironment: 'jsdom',
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest',
+      '^.+\\.(ts|tsx)$': 'ts-jest',
+      '^.+\\.jsx?$': 'babel-jest',
+      '^.+\\.js$': 'jest-transform-stub',
+    },
+   
+  };
