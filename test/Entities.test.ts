d9a0a69ca4530b3285660429564c4b100c4e3f4b
mateoquaglia <117778104+mateoquaglia@users.noreply.github.com>
2024-05-07 15:02:23 -0300

    Update Entities.test.ts

--- test/Entities.test.ts
+++ test/Entities.test.ts
@@ -1,1 +1,25 @@
+import '@testing-library/jest-dom';
+import { collision } from '../js/melonJS-min.js';
+import { NonCollidingEntity } from '../js/entities/entities.js';
 
+describe('NonCollidingEntity tests', () => {
+    it('actualizar la pos correctamente', () => {
+        
+        const entity = new NonCollidingEntity(10, 20);
+        expect(entity.pos.x).toBe(10);
+        expect(entity.pos.y).toBe(20);
+        expect(entity.alwaysUpdate).toBe(true);
+        expect(entity.body.gravity).toBe(0);
+        expect(entity.type).toBe('objeto');
+        expect(entity.body.collisionType).toBe(collision.types.NO_OBJECT);
+    });
+
+    it('should update position correctly', () => {
+        
+        const entity = new NonCollidingEntity(10, 20);
+        entity.update(1000); // Simula el paso de 1 segundo
+        expect(entity.pos.x).toBeLessThan(10);
+    });
+
+    //  otros métodos y comportamientos
+});
